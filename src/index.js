3dbb52daa8f64b7f8e36bd32dc06058e69455d60
bOOOOc <63030160+bOOOOcG@users.noreply.github.com>
2024-08-23 20:57:36 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import LuatssProGamer from './LuatssProGamer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LuatssProGamer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
